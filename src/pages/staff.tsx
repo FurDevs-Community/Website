b6afa432b315676937d6e4e3e15867b5e63df651
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-09-08 19:48:12 -0500

    Staff Page

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -1,21 +1,33 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import Staff from "../components/Staff/Staff";
+import { api } from "../settings";
+import styles from "./../styles/Staff.module.scss";
 
 export default function staff({}): ReactElement {
-    const [staffTeam, setStaffTeam] = useState(null);
+    const [staffTeam, setStaffTeam] = useState([]);
     const [loading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     const fetchStaffTeam = async () => {
-    //         fetch(`${api}/latest/fdevs/staffMembers`).then(async (res) => {
-    //             setStaffTeam(await res.json());
-    //         });
-    //     };
-    //     fetchStaffTeam().then(() => {
-    //         setLoading(false);
-    //     });
-    // }, []);
+    useEffect(() => {
+        const fetchStaffTeam = async () => {
+            fetch(`${api}/latest/fdevs/staffMembers`)
+                .then(async (res) => {
+                    setStaffTeam(await res.json());
+                })
+                .catch(() => {});
+        };
+        fetchStaffTeam().then(() => {
+            setLoading(false);
+        });
+    }, []);
     return (
-        <div>
+        <div className={styles.heading}>
             <h1>Our Staff Team</h1>
+            {staffTeam.length == 0 && loading ? (
+                <h2>LOADING</h2>
+            ) : (
+                staffTeam.map((staff) => (
+                    <Staff color={staff.color} imageURL={staff.imageURL} staffName={staff.username} role={staff.role} />
+                ))
+            )}
         </div>
     );
 }
