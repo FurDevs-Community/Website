f4f451969198cf97fb501967722ed0719ac9ea61
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-02-06 21:08:13 -0600

    Coming Soon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,14 @@
-import { featuresList } from "../components/Featuring/Features";
-import Staff from "../components/Staff/Staff";
-import styles from "./../styles/Home.module.scss";
-import Feature from "../components/Featuring/Feature";
-import { useEffect, useState } from "react";
-import { api } from "../settings";
-import Typings from "../components/Utils/Typings";
+import styles from "../styles/Home.module.scss";
+import Link from 'next/link'
 
 export default function Home() {
-    const [staffTeam, setStaffTeam] = useState(null);
-    const [loading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     const fetchStaffTeam = async () => {
-    //         fetch(`${api}/latest/fdevs/staffMembers`).then(async (res) => {
-    //             setStaffTeam(await res.json());
-    //         });
-    //     };
-    //     fetchStaffTeam().then(() => {
-    //         setLoading(false);
-    //     });
-    // }, []);
     return (
         <div className={styles.home}>
-            <section className={styles.heading}>
-                <div>
-                    <h2>A Furry/Programming Community</h2>
-                    <p>
-                        <Typings
-                            text="FurDevs is a Technology/Programming Community where we discuss about Technology as well as Programming. Our goal is to help make your project whether it's your first or your million-th project. We're willing to help you out as well as bring long time friendships with others in the server."
-                            ms={75}
-                        />
-                    </p>
-                    <div className={styles.buttons}>
-                        <button className={`grow`}>Learn More</button>
-                        <button className={`grow`}>Join our Discord</button>
-                    </div>
-                    <div className={styles.icons}>
-                        <i className="fab fa-node-js" />
-                        <i className="fab fa-java" />
-                        <i className="fab fa-php" />
-                        <i className="fas fa-cogs" />
-                        <i className="fab fa-js" />
-                        <i className="fab fa-python" />
-                        <i className="fab fa-rust" />
-                    </div>
-                </div>
-                <div>
-                    <img src="/images/pawComputer.svg" alt="" />
-                </div>
-            </section>
+            <h1>Hello There</h1>
+            <h2>Our Website is currently being reworked on</h2>
+            <Link href="https://discord.gg/RkMCWWFSBg">
+                <button>Come Join Our Server</button>
+            </Link>
         </div>
     );
 }
